5ccc787a1147d347baa0d76fcdff254cf5ae26bd
Hitonoriol <gorgilike@mail.ru>
2022-09-02 16:44:29 +0300

    Generalize auto scroll focus listener

--- core/src/hitonoriol/madsand/gui/widgets/AutoFocusScrollPane.java
+++ core/src/hitonoriol/madsand/gui/widgets/AutoFocusScrollPane.java
@@ -1,23 +1,12 @@
 package hitonoriol.madsand.gui.widgets;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 
 public class AutoFocusScrollPane extends ScrollPane {
 
 	public AutoFocusScrollPane(Actor actor) {
 		super(actor);
-		addListener(new InputListener() {
-			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-				getStage().setScrollFocus(AutoFocusScrollPane.this);
-			}
-
-			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-				if (getStage() != null)
-					getStage().setScrollFocus(null);
-			}
-		});
+		addListener(new ScrollFocusListener(this));
 	}
 }
---
+++ core/src/hitonoriol/madsand/gui/widgets/ScrollFocusListener.java
@@ -0,0 +1,26 @@
+package hitonoriol.madsand.gui.widgets;
+
+import static hitonoriol.madsand.MadSand.getStage;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+
+public class ScrollFocusListener extends InputListener {
+	private Actor actor;
+	
+	public ScrollFocusListener(Actor actor) {
+		this.actor = actor;
+	}
+	
+	@Override
+	public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+		getStage().setScrollFocus(actor);
+	}
+
+	@Override
+	public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+		if (getStage() != null)
+			getStage().setScrollFocus(null);
+	}
+}
