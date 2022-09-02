7d88dca893d5c4602e9b512b6de9f3ce7c81056d
Hitonoriol <gorgilike@mail.ru>
2022-09-02 15:41:06 +0300

    Define SelectBox style
    + Misc style definition cleanup

--- core/src/hitonoriol/madsand/gui/GuiSkin.java
+++ core/src/hitonoriol/madsand/gui/GuiSkin.java
@@ -14,11 +14,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox.CheckBoxStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -39,29 +41,29 @@ public class GuiSkin {
 	public static final Drawable dialogBackground = setMinSize(Resources.loadNinePatch("gui/bg"), 100, 50);
 
 	private static Map<Integer, LabelStyle> labelStyles = new HashMap<>();
-
+	
 	static Skin init() {
 		createInitialColorDrawable();
 		skin.add("default", getFont(Gui.FONT_S));
 
 		Slider.SliderStyle sliderStyle = new Slider.SliderStyle();
-		sliderStyle.background = setMinSize(GuiSkin.getColorDrawable(Color.DARK_GRAY), 10, 30);
-		sliderStyle.knob = setMinSize(GuiSkin.getColorDrawable(Color.GRAY), 10, 30);
+		sliderStyle.background = setMinSize(getColorDrawable(Color.DARK_GRAY), 10, 30);
+		sliderStyle.knob = setMinSize(getColorDrawable(Color.GRAY), 10, 30);
 		skin.add("default-horizontal", sliderStyle);
 
 		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-		textButtonStyle.up = GuiSkin.getColorDrawable(Color.GRAY);
-		textButtonStyle.down = GuiSkin.getColorDrawable(Color.DARK_GRAY);
-		textButtonStyle.over = GuiSkin.getColorDrawable(GuiColors.MOUSEOVER);
-		textButtonStyle.font = skin.getFont("default");
-		textButtonStyle.disabled = GuiSkin.getColorDrawable(GuiColors.BUTTON_DISABLED);
+		textButtonStyle.up = getColorDrawable(Color.GRAY);
+		textButtonStyle.down = getColorDrawable(Color.DARK_GRAY);
+		textButtonStyle.over = getColorDrawable(GuiColors.MOUSEOVER);
+		textButtonStyle.font = getDefaultFont();
+		textButtonStyle.disabled = getColorDrawable(GuiColors.BUTTON_DISABLED);
 		skin.add("default", textButtonStyle);
 
-		Label.LabelStyle labelStyle = GuiSkin.createLabelStyle(Gui.FONT_S);
+		Label.LabelStyle labelStyle = createLabelStyle(Gui.FONT_S);
 		skin.add("default", labelStyle);
 
 		Window.WindowStyle windowStyle = new Window.WindowStyle();
-		windowStyle.background = GuiSkin.getColorDrawable(Color.LIGHT_GRAY);
+		windowStyle.background = getColorDrawable(Color.LIGHT_GRAY);
 		windowStyle.stageBackground = darkBackground;
 		windowStyle.titleFontColor = Color.WHITE;
 		windowStyle.titleFont = getFont(Gui.FONT_M);
@@ -70,14 +72,15 @@ public class GuiSkin {
 		TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
 		textFieldStyle.font = getFont(Gui.FONT_S);
 		textFieldStyle.fontColor = Color.WHITE;
-		textFieldStyle.background = GuiSkin.getColorDrawable(Color.DARK_GRAY);
+		textFieldStyle.background = getColorDrawable(Color.DARK_GRAY);
 		textFieldStyle.background.setMinHeight(35.0f);
-		textFieldStyle.selection = GuiSkin.getColorDrawable(Color.LIGHT_GRAY);
-		textFieldStyle.cursor = setMinSize(GuiSkin.getColorDrawable(Color.GRAY), 1f,
-				textFieldStyle.background.getMinHeight());
+		textFieldStyle.selection = getColorDrawable(Color.LIGHT_GRAY);
+		textFieldStyle.cursor = setMinSize(getColorDrawable(Color.GRAY), 1f, textFieldStyle.background.getMinHeight());
 		skin.add("default", textFieldStyle);
 
 		ScrollPane.ScrollPaneStyle scrollStyle = new ScrollPane.ScrollPaneStyle();
+		scrollStyle.hScrollKnob = setMinSize(getColorDrawable(Color.DARK_GRAY), 5, 10);
+		scrollStyle.hScroll = setMinSize(getColorDrawable(Color.DARK_GRAY), 2);
 		skin.add("default", scrollStyle);
 
 		TextTooltip.TextTooltipStyle tooltipStyle = new TextTooltip.TextTooltipStyle();
@@ -85,23 +88,47 @@ public class GuiSkin {
 		tooltipStyle.label = labelStyle;
 		skin.add("default", tooltipStyle);
 
-		Drawable barKnob = GuiSkin.getColorDrawable(Color.DARK_GRAY);
+		Drawable barKnob = getColorDrawable(Color.DARK_GRAY);
 		barKnob.setMinWidth(5);
 
-		ProgressBar.ProgressBarStyle barStyle = new ProgressBarStyle(GuiSkin.getColorDrawable(Color.GRAY), barKnob);
+		ProgressBar.ProgressBarStyle barStyle = new ProgressBarStyle(getColorDrawable(Color.GRAY), barKnob);
 		barStyle.knobBefore = barStyle.knob;
 		skin.add("default-horizontal", barStyle);
 
 		CheckBox.CheckBoxStyle boxStyle = new CheckBoxStyle();
-		boxStyle.checkboxOn = setMinSize(GuiSkin.getColorDrawable(Color.DARK_GRAY), BTN_HEIGHT);
-		boxStyle.checkboxOnOver = setMinSize(GuiSkin.getColorDrawable(GuiColors.LIGHT_DARK_GRAY), BTN_HEIGHT);
-		boxStyle.checkboxOff = setMinSize(GuiSkin.getColorDrawable(Color.GRAY), BTN_HEIGHT);
-		boxStyle.checkboxOver = setMinSize(GuiSkin.getColorDrawable(GuiColors.MOUSEOVER), BTN_HEIGHT);
+		boxStyle.checkboxOn = setMinSize(getColorDrawable(Color.DARK_GRAY), BTN_HEIGHT);
+		boxStyle.checkboxOnOver = setMinSize(getColorDrawable(GuiColors.LIGHT_DARK_GRAY), BTN_HEIGHT);
+		boxStyle.checkboxOff = setMinSize(getColorDrawable(Color.GRAY), BTN_HEIGHT);
+		boxStyle.checkboxOver = setMinSize(getColorDrawable(GuiColors.MOUSEOVER), BTN_HEIGHT);
 		boxStyle.font = textButtonStyle.font;
 		skin.add("default", boxStyle);
+
+		List.ListStyle listStyle = new List.ListStyle();
+		listStyle.background = textButtonStyle.up;
+		listStyle.down = textButtonStyle.down;
+		listStyle.font = getDefaultFont();
+		listStyle.over = textButtonStyle.over;
+		listStyle.selection = getColorDrawable(Color.DARK_GRAY);
+		listStyle.selection.setTopHeight(10);
+		listStyle.selection.setBottomHeight(10);
+		skin.add("default", listStyle);
+		
+		SelectBox.SelectBoxStyle selBoxStyle = new SelectBox.SelectBoxStyle();
+		selBoxStyle.background = textButtonStyle.up;
+		selBoxStyle.backgroundOpen = textButtonStyle.checkedDown;
+		selBoxStyle.backgroundOver = textButtonStyle.over;
+		selBoxStyle.backgroundDisabled = textButtonStyle.disabled;
+		selBoxStyle.font = getDefaultFont();
+		selBoxStyle.scrollStyle = scrollStyle;
+		selBoxStyle.listStyle = listStyle;
+		skin.add("default", selBoxStyle);
 		return skin;
 	}
 
+	public static BitmapFont getDefaultFont() {
+		return skin.getFont("default");
+	}
+	
 	private static void createInitialColorDrawable() {
 		Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGB888);
 		pixmap.setColor(Color.WHITE);
@@ -111,8 +138,8 @@ public class GuiSkin {
 
 	public static LabelStyle getLabelStyle(int size) {
 		if (!labelStyles.containsKey(size))
-			labelStyles.put(size, GuiSkin.createLabelStyle(size));
-
+			labelStyles.put(size, createLabelStyle(size));
+	
 		return labelStyles.get(size);
 	}
 
@@ -139,10 +166,10 @@ public class GuiSkin {
 		Drawable knob = getColorDrawable(color);
 		knob.setMinWidth(5);
 		ProgressBarStyle style = new ProgressBarStyle(transparent ? transparency : getColorDrawable(Color.GRAY), knob);
-
+	
 		style.background.setMinWidth(width);
 		style.background.setMinHeight(height);
-
+	
 		style.knob.setMinHeight(height);
 		style.knobBefore = style.knob;
 		style.knobBefore.setMinHeight(height);
@@ -152,7 +179,7 @@ public class GuiSkin {
 	public static ProgressBarStyle createProgressBarStyle(float width, float height, Color color) {
 		return createProgressBarStyle(width, height, color, false);
 	}
-
+	
 	public static Skin get() {
 		return skin;
 	}
