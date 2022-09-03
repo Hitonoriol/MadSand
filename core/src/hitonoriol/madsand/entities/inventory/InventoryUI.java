ff9e25f12b62565429856f9bd9a5f2d86b750a8e
Hitonoriol <gorgilike@mail.ru>
2022-09-03 22:27:48 +0300

    Sort & filter items in inventory UI
    + Inventory UI layout adjustments

--- core/src/hitonoriol/madsand/entities/inventory/InventoryUI.java
+++ core/src/hitonoriol/madsand/entities/inventory/InventoryUI.java
@@ -1,5 +1,7 @@
 package hitonoriol.madsand.entities.inventory;
 
+import static com.badlogic.gdx.Gdx.graphics;
+
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -14,21 +16,21 @@ import hitonoriol.madsand.gui.Widgets;
 import hitonoriol.madsand.gui.widgets.AutoFocusScrollPane;
 
 public class InventoryUI extends GameDialog {
-	private static final int ITEMS_PER_ROW = 5;
-	private static final int WIDTH = 400;
-	private static final int HEIGHT = 500;
-	private static final int OFFSET = 5;
+	private static final float ITEM_PAD = 7, PAD = 10;
+	private float BODY_WIDTH = 0.6f * graphics.getWidth();
+	private final float BODY_HEIGHT = 0.7f * graphics.getHeight();
+	private final int ITEMS_PER_ROW = (int) ((BODY_WIDTH - ITEM_PAD * 2 * (BODY_WIDTH / InventoryUICell.SIZE))
+			/ InventoryUICell.SIZE);
 
 	private static final float BUTTON_PADDING = 5;
 	private static final float BUTTON_WIDTH = 250;
 	private static final float BUTTON_HEIGHT = 50;
 
-	private Table invContainer;
-	private Table invTable;
-	private AutoFocusScrollPane invScroll;
-	private Table invScrollTable;
-	private Label header;
-	private TextButton craftMenuButton;
+	private Table invContainer = new Table();
+	private Table itemTable = new Table();
+	private AutoFocusScrollPane invScroll = new AutoFocusScrollPane(itemTable);;
+	private Label header = new Label("", Gui.skin);
+	private ItemSearchPanel searchPanel = new ItemSearchPanel(true);
 
 	private Inventory inventory;
 	private int items;
@@ -36,52 +38,53 @@ public class InventoryUI extends GameDialog {
 	public InventoryUI(Inventory inventory) {
 		super(Gui.overlay);
 		this.inventory = inventory;
-		invContainer = new Table();
-		invScrollTable = new Table();
-		invTable = new Table();
-		craftMenuButton = new TextButton("Crafting", Gui.skin);
-		invTable.row();
-
-		header = new Label("", Gui.skin);
+		BODY_WIDTH = ITEMS_PER_ROW * InventoryUICell.SIZE + ITEMS_PER_ROW * ITEM_PAD * 2;
 		Gui.setFontSize(header, Gui.FONT_L);
-
-		invTable.setBackground(GuiSkin.darkBackground);
-		invTable.align(Align.topLeft);
-		invTable.setWidth(WIDTH);
-		invScrollTable.add(invTable);
-
-		invScroll = new AutoFocusScrollPane(invScrollTable);
-		invScroll.setHeight(HEIGHT);
-		invScroll.setWidth(WIDTH + OFFSET);
+		itemTable.setBackground(GuiSkin.darkBackground);
 		invScroll.setOverscroll(false, false);
 		invScroll.setScrollingDisabled(true, false);
 
-		invContainer.add(header).pad(10).fillY().align(Align.center).row();
-		invContainer.add(invScroll).size(WIDTH, HEIGHT).row();
-		invContainer.add(craftMenuButton)
-				.size(BUTTON_WIDTH, BUTTON_HEIGHT)
-				.align(Align.center)
-				.pad(BUTTON_PADDING)
+		Table body = Widgets.table();
+		Table buttonTable = Widgets.table();
+		header.setAlignment(Align.center);
+		body.add(header).colspan(2).width(BODY_WIDTH).padBottom(PAD).align(Align.right).row();
+		body.add(searchPanel).padRight(PAD).align(Align.top);
+		body.add(invScroll).size(BODY_WIDTH, BODY_HEIGHT);
+		invContainer.add(body).align(Align.left).row();
+		invContainer.add(buttonTable).colspan(2)
+				.padTop(BUTTON_PADDING)
+				.width(BODY_WIDTH)
+				.align(Align.right)
 				.row();
-		invContainer.add(Gui.setAction(Widgets.button("Close [E]"), () -> hide()))
-				.size(BUTTON_WIDTH, BUTTON_HEIGHT).row();
 		invContainer.setFillParent(true);
 
 		super.add(invContainer);
 		super.setBackground(GuiSkin.transparency);
 
+		TextButton craftMenuButton = new TextButton("Crafting", Gui.skin);
+		buttonTable.defaults()
+				.size(BUTTON_WIDTH, BUTTON_HEIGHT)
+				.align(Align.center)
+				.pad(BUTTON_PADDING);
+		buttonTable.add(craftMenuButton).row();
+		buttonTable.add(Gui.setAction(Widgets.button("Close [E]"), () -> hide()));
+
 		Gui.setAction(craftMenuButton, () -> {
 			Gui.openCraftMenu(0);
 			Screens.Game.screen().onShow(() -> refresh());
 		});
+		searchPanel.onChange(() -> refresh());
 		refresh();
 	}
 
 	private void refresh() {
 		items = 0;
-		invTable.clear();
+		itemTable.clear();
 		inventory.getItems().stream()
+				.filter(searchPanel.getFilter())
+				.sorted(searchPanel.getSort())
 				.forEach(item -> addItem(item));
+		itemTable.align(items > ITEMS_PER_ROW ? Align.top : Align.topLeft);
 		refreshTitle();
 	}
 
@@ -93,8 +96,8 @@ public class InventoryUI extends GameDialog {
 		cell.refreshEquippedStatus();
 		++items;
 		if (items % ITEMS_PER_ROW == 1)
-			invTable.row();
-		invTable.add(cell);
+			itemTable.row();
+		itemTable.add(cell).pad(ITEM_PAD);
 	}
 
 	private void addItem(Item item) {
@@ -112,15 +115,12 @@ public class InventoryUI extends GameDialog {
 
 	void openingContextMenu(InventoryUICell cell) {
 		Object obj = getUserObject();
-		if (!(obj instanceof InventoryUICell))
-			setUserObject(cell);
-		else {
+		if (obj instanceof InventoryUICell)
 			((InventoryUICell) obj).closeContextMenu();
-			setUserObject(null);
-		}
+		setUserObject(cell);
 	}
 
 	void setHeader(String str) {
-		header.setText(str);
+		header.setText("Total weight: " + str);
 	}
 }
