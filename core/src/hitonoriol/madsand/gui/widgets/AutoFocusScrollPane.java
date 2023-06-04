4e3dfc5ebfe9b2af958c4fb62c2392c6a93c7bae
Hitonoriol <gorgilike@mail.ru>
2023-06-04 03:23:41 +0300

    Propagate size changes to child

--- core/src/hitonoriol/madsand/gui/widgets/AutoFocusScrollPane.java
+++ core/src/hitonoriol/madsand/gui/widgets/AutoFocusScrollPane.java
@@ -3,10 +3,24 @@ package hitonoriol.madsand.gui.widgets;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 
+import hitonoriol.madsand.util.Utils;
+
 public class AutoFocusScrollPane extends ScrollPane {
 
 	public AutoFocusScrollPane(Actor actor) {
 		super(actor);
+		setScrollingDisabled(true, false);
 		addListener(new ScrollFocusListener(this));
+		setWidth(actor.getWidth());
+	}
+	
+	@Override
+	protected void sizeChanged() {
+		var actor = getActor();
+		if (actor != null) {
+			Utils.out("ScrollPane: Updating child width: %f -> %f", actor.getWidth(), getWidth());
+			actor.setWidth(getWidth());
+		}
+		super.sizeChanged();
 	}
 }
